4039f2e8860312035057756e70c4fcc9b9b32aac
Your Name <your@email.com>
2025-07-24 04:06:36 +0530

    landing page

--- Newfrontend/next.config.mjs
+++ Newfrontend/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during builds
+  },
   images: {
     remotePatterns: [
       {
