76bea87bb448fe26e7a6c49652db86691b7cd119
Amaan Mirza <amaanmirza287@gmail.com>
2025-07-26 05:21:22 +0530

    api middleware fixed

--- Backend/src/middleware/api.middleware.ts
+++ Backend/src/middleware/api.middleware.ts
@@ -4,13 +4,14 @@ import { sendError } from "../utils/response.util";
 
 export function apiCheck() {
   return (req: Request, res: Response, next: NextFunction): void => {
-    const { api } = req.body;
-    if (!api) {
+    const apiKey = req.headers["x-api-key"] || req.headers["api-key"];
+
+    if (!apiKey) {
       sendError(res, "No Api provided", "Unauthorized", 401);
       return;
     }
     try {
-      if (api != config.backendApiSecret) {
+      if (apiKey != config.backendApiSecret) {
         sendError(res, "Forbidden", "Forbidden", 403);
         return;
       }
